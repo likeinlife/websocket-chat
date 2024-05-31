2208ae13c762b6041f2444c9dbb9509d5a6275e9
likeinlife <likeinlife@outlook.com>
2024-05-31 14:27:54 +0300

    feat(domain.baseevent): from_dict method

--- sciarticle_task/domain/events/_base.py
+++ sciarticle_task/domain/events/_base.py
@@ -1,15 +1,18 @@
-from dataclasses import (
-    dataclass,
-    field,
-)
+import typing as tp
+import uuid
+from dataclasses import dataclass, field, fields
 from datetime import datetime
-from uuid import (
-    UUID,
-    uuid4,
-)
+
+T = tp.TypeVar("T", bound="BaseEvent")
 
 
 @dataclass
 class BaseEvent:
-    event_id: UUID = field(default_factory=uuid4, kw_only=True)
+    event_id: uuid.UUID = field(default_factory=uuid.uuid4, kw_only=True)
     occurred_at: datetime = field(default_factory=datetime.now, kw_only=True)
+
+    @classmethod
+    def from_dict(cls: type[T], data: dict[str, tp.Any]) -> T:
+        ignore_names = {"event_id", "occurred_at"}
+        names = {f.name for f in fields(cls)}
+        return cls(**{k: v for k, v in data.items() if k in names and k not in ignore_names})

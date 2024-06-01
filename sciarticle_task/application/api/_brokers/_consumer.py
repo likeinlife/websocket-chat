02857690eeb254da37cc1581a77024698aed172e
likeinlife <likeinlife@outlook.com>
2024-06-01 22:13:11 +0300

    fix(application, domain): minor

--- sciarticle_task/application/api/_brokers/_consumer.py
+++ sciarticle_task/application/api/_brokers/_consumer.py
@@ -5,24 +5,18 @@ from logic.events.entities import NewMessageFromBrokerEvent
 from logic.mediator import Mediator
 from logic.serializer import BaseEventSerializer
 
-from ._errors import UnexpectedMessageError
-
 mediator: Mediator = Provide[Container.logic.mediator]
 serializer: BaseEventSerializer = Provide[Container.logic.serializer]
 
 
 @inject
 async def message_consumer(body: dict) -> None:
-    event = serializer.deserialize(body)
-    if not isinstance(event, NewMessageEvent):
-        raise UnexpectedMessageError(event.__class__.__name__)
-    await mediator.event_mediator.publish(
-        [
-            NewMessageFromBrokerEvent(
-                message_text=event.message_text,
-                chat_id=event.chat_id,
-                message_id=event.message_id,
-                user_name=event.user_name,
-            ),
-        ],
+    event = serializer.deserialize(body, NewMessageEvent)
+    await mediator.event_mediator.handle(
+        NewMessageFromBrokerEvent(
+            message_text=event.message_text,
+            chat_id=event.chat_id,
+            message_id=event.message_id,
+            user_name=event.user_name,
+        ),
     )
--- sciarticle_task/application/api/_routes/chat/_messages.py
+++ sciarticle_task/application/api/_routes/chat/_messages.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from application.api.dependencies import user
 from container import Container
 from dependency_injector.wiring import Provide, inject
@@ -19,7 +21,7 @@ async def post_message(
     user: User = Depends(user.get_user),
     mediator: Mediator = Depends(Provide[Container.logic.mediator]),
 ) -> PostMessageResponse:
-    result = await mediator.command_mediator.handle_command(
+    result = await mediator.command_mediator.handle(
         CreateMessageCommand(chat_id=body.chat_id, message_text=body.text, user=user),
     )
-    return PostMessageResponse(id=result.id)
+    return PostMessageResponse(id=UUID(result.id))

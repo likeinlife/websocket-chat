ebe1af875dc9385fd05646de72c0afb8fc6f5ce9
likeinlife <likeinlife@outlook.com>
2024-06-02 19:22:52 +0300

    minor(application.routes.events): change endpoint name from `events` to `event`

--- sciarticle_task/application/api/_routes/events/_register.py
+++ sciarticle_task/application/api/_routes/events/_register.py
@@ -4,7 +4,7 @@ from . import _events
 
 
 def register(router: APIRouter) -> None:
-    _router = APIRouter(prefix="/events", tags=["events"])
+    _router = APIRouter(prefix="/event", tags=["event"])
     _router.include_router(_events.router)
 
     router.include_router(_router)
